dc79c32de8cc346d424a5af770cbaa80f4a2012c
rohan <rohanluthra14@gmail.com>
2020-04-01 18:08:36 +0530

    Build added

--- items/generate.sh
+++
@@ -1,7 +0,0 @@
-// go
-protoc src/proto/*.proto --go_out=plugins=grpc:.
-// js
-protoc src/proto/*.proto --js_out=import_style=commonJS,binary:./src/model/.
-
-//ts
-protoc --plugin=protoc-gen-ts=../node_modules/.bin/protoc-gen-ts src/proto/*.proto --ts_out=import_style=commonJS,binary:.
